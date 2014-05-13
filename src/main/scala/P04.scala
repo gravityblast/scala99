3d5fb075c848c0d9042a92942dc2d92969a859f0
Andrea Franz <andrea@gravityblast.com>
2014-05-13 01:19:28 +0100

    optimized P04

--- src/main/scala/P04.scala
+++ src/main/scala/P04.scala
@@ -4,10 +4,18 @@
 // res0: Int = 6
 
 object P04 {
-  def length[A](items: List[A]): Int = items match {
-    case Nil       => 0
-    case _ :: tail => 1 + length(tail)
+  def length[A](items: List[A]): Int = {
+    def f[A](sum: Int, items: List[A]): Int = items match {
+      case Nil       => sum
+      case _ :: tail => f(sum + 1, tail)
+    }
+    f(0, items)
   }
+
+  // def length[A](items: List[A]): Int = items match {
+  //   case Nil       => 0
+  //   case _ :: tail => 1 + length(tail)
+  // }
 }
 
 
